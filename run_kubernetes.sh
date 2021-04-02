50d6b6d249739e832ebf69b3a4e4e594c92ee6c0
rahairston <rahairston2014@verizon.net>
2021-04-02 13:18:12 -0500

    updates

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,6 +5,7 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="rahairston/m1"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="rahairston/ryan-udacity-microservices"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username rahairston
+docker tag ryan-udacity-microservices $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
